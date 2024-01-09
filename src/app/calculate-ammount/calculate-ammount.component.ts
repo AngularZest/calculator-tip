1090d84ff91babd8b7c45bfad377bf1e6245d73a
vikram <vikram.angular@zestgeek.com>
2024-01-09 16:10:45 +0530

    update

--- src/app/calculate-ammount/calculate-ammount.component.ts
+++ src/app/calculate-ammount/calculate-ammount.component.ts
@@ -20,63 +20,77 @@ export class CalculateAmmountComponent {
   numericValue: any
   finalAmount: any
   cleanedValue: any
+  itemNameofshow:any
   tip: any
+  itemName:any
   isActive:any
   tipvalue:any
   constructor(private fb: FormBuilder) {
     this.calculate = this.fb.group({
       price: [''],
-      total: [0.00],
-      gst: [0.00],
-      quantity: [0.00],
+      total: [''],
+      gst: [''],
+      quantity: [''],
       finaltotal: [0.00],
       tip: ['']
     });
   }
   pricelist = [
     {
+      id:1,
       Dish: 'Pizza',
       price: 120
     },
     {
-      Dish: 'Burgur',
+      id:2,
+      Dish: 'French Fries',
       price: 50
     },
     {
-      Dish: 'Momose',
+      id:3,
+      Dish: 'Machurian',
       price: 90
     },
     {
-      Dish: 'Pasta',
+      id:4,
+      Dish: 'Italian Pasta',
       price: 130
     }
   ]
   getPrice(price: any) {
-
-    const selectedValue = price.target.value;
-    this.cleanedValue = selectedValue.replace(/[^a-zA-Z]/g, '');
-    this.numericValue = parseFloat(price.target.value.replace(/[^\d]/g, ''));
-    if (!isNaN(this.numericValue)) {
-      this.itemPrice = this.numericValue;
-    } else {
-      console.error('Invalid numeric value');
-    }
+    this.pricelist.forEach((res:any)=>{
+      if(price.target.value == res.price){
+        this.itemPrice = res?.price
+        this.itemName = res?.Dish
+      }
+    })
+  
+    // const selectedValue = price.target.value;
+    // this.cleanedValue = selectedValue.replace(/[^a-zA-Z]/g, '');
+    // this.numericValue = parseFloat(price.target.value.replace(/[^\d]/g, ''));
+    // if (!isNaN(this.numericValue)) {
+    //   this.itemPrice = this.numericValue;
+    // } else {
+    //   console.error('Invalid numeric value');
+    // }
   }
   value(value:any){
     this.isActive = value
     this.tipvalue = value
+    this.calculate.controls['tip'].reset();
   }
-  getTotal() {
-    const price = this.itemPrice;
-    const quantity = this.calculate.value.quantity
-    const quantityAmount = price * quantity
-    this.calculate.patchValue({
-      total: quantityAmount ? quantityAmount : ''
-    })
-  }
+  // getTotal() {
+  //   const price = this.itemPrice;
+  //   const quantity = this.calculate.value.quantity
+  //   const quantityAmount = price * quantity
+  //   this.calculate.patchValue({
+  //     total: quantityAmount ? quantityAmount : ''
+  //   })
+  // }
 
   calculateammount() {
     // this.total = this.calculate?.value?.total;
+    this.itemNameofshow = this.itemName
     this.quantity = this.calculate?.value?.quantity
     this.tip = this.calculate?.value?.tip ? this.calculate?.value?.tip : this.tipvalue;
     this.price = this?.itemPrice;
@@ -96,6 +110,14 @@ export class CalculateAmmountComponent {
     })
   }
   handleReset(){
-    this.calculate.reset()
+    this.calculate.reset();
+    this.tipvalue = '';
+    this.itemPrice = ''
+    this.itemNameofshow = '0 . 00';
+    this.quantity = '0 . 00';
+    this.tip = '0 . 00';
+    this.gst = '0 . 00';
+    this.finalAmount = '0 . 00 ';
+    this.isActive = null; // Reset the active tip button
   }
 }
